c680cf832c0c0e63785cf46852698c5d4e4c39d6
小孙同学Coding <sun412926@gmail.com>
2023-03-24 04:30:05 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -72,7 +72,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
       { text: '友链', link: 'https://blog.sunguoqi.com/links' },
       { text: '留言', link: 'https://blog.sunguoqi.com/comments' },
-      { text: '关于', link: 'https://blog.sunguoqi.com/abouts' },
+      { text: '关于', link: 'https://blog.sunguoqi.com/about' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
