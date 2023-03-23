f4a8a5cbee8fcccc05389eae24a79494310a113d
guoqi.sun <guoqi.sun@nio.com>
2023-03-24 02:23:49 +0800

    :bug: fix vercel deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
-const DOMAIN_NAME = 'xugaoyi.com' // 域名 (不带https)
+const DOMAIN_NAME = 'brain.sunguoqi.com' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
---
+++ docs/.vuepress/dist/assets/js/21.dbb39be3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{326:function(n,w,o){}}]);
