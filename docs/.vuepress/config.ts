c363f7c0cc2323bc464ed2d0f4c6cccef815c223
guoqi.sun <guoqi.sun@nio.com>
2023-03-24 02:52:29 +0800

    :new: 配置gitalk评论

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -329,11 +329,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: '449bcfc52c2232e0f2f1',
+          clientSecret: 'dd27991df2bdb6b7f595b1bbe29edcf1f3cfbb9b',
+          repo: 'Brain', // GitHub 仓库
+          owner: 'sun0225SUN', // GitHub仓库所有者
+          admin: ['sun0225SUN'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
