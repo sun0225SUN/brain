600790f9ce3f95af4585d3e131b55e019cff7aad
guoqi.sun <sun412926@gmail.com>
2023-07-27 00:24:10 +0800

    :bug: link——>script

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
   // 基础路径
   base: "/",
   //头部引入，这里引入的是51la统计
-  head: [["link", { type: "text/javascript", href: "/assets/js/51la.js" }]],
+  head: [["script", { type: "text/javascript", href: "/assets/js/51la.js" }]],
   // 网站信息
   locales: {
     "/": {
