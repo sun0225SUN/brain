82d830a8918ece6df478d04c06243f0b52f4fa03
guoqi.sun <sun412926@gmail.com>
2023-07-27 00:12:48 +0800

    :tada: feat: 添加51la站点统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,6 +5,8 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 export default defineUserConfig({
   // 基础路径
   base: "/",
+  //头部引入，这里引入的是51la统计
+  head: [["link", { type: "text/javascript", href: "/assets/js/51la.js" }]],
   // 网站信息
   locales: {
     "/": {
