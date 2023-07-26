42df1071a54a5ff69a65fe26bb628f9f87b33bd4
guoqi.sun <sun412926@gmail.com>
2023-07-26 18:36:02 +0800

    :zap: something changed

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,6 @@
-import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
-import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   // 基础路径
@@ -19,16 +18,18 @@ export default defineUserConfig({
   // 插件配置
   plugins: [
     searchProPlugin({
+      // 索引全部内容
       indexContent: true,
+      // 为分类和标签添加索引
       customFields: [
-        // {
-        //   getter: (page) => page.frontmatter.category,
-        //   formatter: "分类：$content",
-        // },
-        // {
-        //   getter: (page) => page.frontmatter.tag,
-        //   formatter: "标签：$content",
-        // },
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
       ],
     }),
   ],
