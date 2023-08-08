b148dbceee5fc32689bdc2d2d08fdd342eeacec5
guoqi.sun <sun412926@gmail.com>
2023-08-08 16:54:32 +0800

    :zap: perf: update devDependencies versions of "@vuepress/client" to "2.0.0-beta.66", "vuepress-plugin-search-pro" to "2.0.0-beta.234", and "vuepress-theme-hope" to "2.0.0-beta.234". Additionally, add "@apideck/better-ajv-errors" as a new dependency with version "0.3.6"

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -22,17 +22,6 @@ export default defineUserConfig({
     searchProPlugin({
       // 索引全部内容
       indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
     }),
   ],
   // Enable it with pwa
