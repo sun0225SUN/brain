05a822e0acbfcf296b48cd0e4920efaacf592df4
guoqi.sun <sun412926@gmail.com>
2023-09-07 21:55:22 +0800

    :hammer: chore: refactor hopeTheme settings and fix code tab issue

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,18 +3,10 @@ import { sideBar } from "./sidebar/index.js";
 import { hopeTheme } from "vuepress-theme-hope";
 
 export default hopeTheme({
-  hostname: "https://brain.sunguoqi.com",
-
-  author: {
-    name: "小孙同学",
-    url: "https://blog.sunguoqi.com",
-  },
-
-  logo: "/avatar.png",
-
-  repo: "sun0225SUN/Brain",
-
   docsDir: "/src",
+  repo: "sun0225SUN/Brain",
+  logo: "/avatar.png",
+  hostname: "https://brain.sunguoqi.com",
 
   iconAssets: ["//at.alicdn.com/t/c/font_4184658_5gtozjqek5i.css"],
 
@@ -27,31 +19,22 @@ export default hopeTheme({
       blog: {
         description: "路虽远行则将至，事虽难做则必成！",
         intro: "https://blog.sunguoqi.com/about",
-      },
-      blogLocales: {
-        tutorial: "Tutorial",
+        timeline: "好好学习，天天向上",
       },
       metaLocales: {
-        editLink: "Edit this page on GitHub",
+        editLink: "编辑此页",
       },
     },
   },
 
-  blog: {
-    // 时间线标题
-    timeline: "好好学习，天天向上",
-    // 社交媒体
-  },
-
-  // 全屏按钮
-  fullscreen: false,
-
   plugins: {
-    // 博客设置
     blog: true,
-    // 评论设置
+
+    feed: {
+      rss: true,
+    },
+
     comment: {
-      // @ ts-expect-error: You should generate and use your own comment service
       provider: "Giscus",
       repo: "sun0225SUN/Brain",
       repoId: "R_kgDOJNZRqA",
@@ -63,58 +46,52 @@ export default hopeTheme({
       darkTheme: "dark",
     },
 
-    // all features are enabled for demo, only preserve features you need here
-    // Markdown 增强
     mdEnhance: {
-      card: true,
-      align: true,
-      attrs: true,
-      chart: true,
+      tabs: true,
       codetabs: true,
-      demo: true,
+      chart: true,
       echarts: true,
-      figure: true,
-      gfm: true,
-      imgLazyload: true,
-      imgSize: true,
-      imgMark: true,
-      include: true,
-      katex: true,
-      mark: true,
       mermaid: true,
-      playground: {
-        presets: ["ts", "vue"],
-      },
-      // 幻灯片
-      // presentation: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
+      katex: true,
+      vuePlayground: true,
       sub: true,
       sup: true,
-      tabs: true,
-      vPre: true,
-      vuePlayground: true,
+      demo: true,
+      tasklist: true,
+      card: true,
+      figure: true,
+      imgLazyload: true,
+      imgMark: true,
+      imgSize: true,
+      include: true,
+      attrs: true,
+      presentation: true,
+      mark: true,
+      footnote: true,
+      container: true,
+      align: true,
     },
 
-    // uncomment these if you want a pwa
+    components: {
+      components: [
+        "AudioPlayer",
+        "Badge",
+        "BiliBili",
+        "CodePen",
+        "PDF",
+        "Replit",
+        "StackBlitz",
+        "VideoPlayer",
+        "YouTube",
+      ],
+    },
+
     pwa: {
       favicon: "/avatar.png",
       cacheHTML: true,
       cachePic: true,
       appendBase: true,
+      update: "force",
       apple: {
         icon: "/assets/icon/apple-icon-152.png",
         statusBarColor: "black",
@@ -164,27 +141,6 @@ export default hopeTheme({
           },
         ],
       },
-    },
-
-    // RSS
-    feed: {
-      rss: true,
-    },
-
-    // 内置组件
-    components: {
-      // 你想使用的组件
-      components: [
-        "AudioPlayer",
-        "Badge",
-        "BiliBili",
-        "CodePen",
-        "PDF",
-        "Replit",
-        "StackBlitz",
-        "VideoPlayer",
-        "YouTube",
-      ],
     },
   },
 });
