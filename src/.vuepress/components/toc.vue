bff9850e7591c9d9cc30505962e0f94dfb278c42
guoqi.sun <sun412926@gmail.com>
2023-10-19 09:57:55 +0800

    :sparkles: feat: "Add flip animation to display front and back images on hover"

--- src/.vuepress/components/toc.vue
+++ src/.vuepress/components/toc.vue
@@ -1,11 +1,12 @@
 <template>
   <TOC>
     <template #after>
-      <img
-        class="wp"
-        src="https://blog.sunguoqi.com/upload/wx1.png"
-        alt="微信公众号"
-      />
+      <div id="flip-wrapper">
+        <div id="flip-content">
+          <div class="face"></div>
+          <div class="back face"></div>
+        </div>
+      </div>
     </template>
   </TOC>
 </template>
@@ -15,9 +16,51 @@ import TOC from "vuepress-theme-hope/modules/info/components/TOC.js";
 </script>
 
 <style lang="scss" scoped>
-.wp {
+#flip-wrapper {
+  position: relative;
   width: 250px;
-  margin-top: 20px;
+  height: 70px;
+  z-index: 1;
+  margin-top: 30px;
+}
+
+#flip-wrapper {
+  -webkit-perspective: 1000;
+  perspective: 1000;
+}
+
+#flip-content {
+  width: 100%;
+  height: 100%;
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  transition: cubic-bezier(0, 0, 0, 1.29) 0.5s;
+}
+
+#flip-wrapper:hover #flip-content {
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background: url(https://blog.sunguoqi.com/upload/wx1.png) center center
+    no-repeat;
+  background-size: 100%;
   border-radius: 5px;
+}
+
+.back.face {
+  display: block;
+  -webkit-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+  box-sizing: border-box;
+  background: url(https://blog.sunguoqi.com/upload/wx2.png) center center
+    no-repeat;
+  background-size: 100%;
 }
 </style>
